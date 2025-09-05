935d509f8e21850a7713c88a0b7026bdc5dc52ba
vibhorgupta00ta <vibhorgup00ta@gmail.com>
2025-09-05 13:11:40 +0000

    conversionOperations

---
+++ basics/03_conversionOperations.js
@@ -0,0 +1,23 @@
+let score=true
+
+//console.log(typeof score)
+//console.log(typeof (score))
+
+let valyeInNumber=Number(score)
+//console.log(typeof valyeInNumber)
+//console.log(valyeInNumber);
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1; false=>0
+
+let isLoggedIn="vibhor"
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+//1=>true;0=>false
+//""=>false
+//"vibhor"=>true
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber)
