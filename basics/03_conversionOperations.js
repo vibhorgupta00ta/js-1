55cf025e115b9781adf6abbe32927078732064c9
vibhorgupta00ta <vibhorgup00ta@gmail.com>
2025-09-05 13:29:54 +0000

    ConversionOperations

--- basics/03_conversionOperations.js
+++ basics/03_conversionOperations.js
@@ -13,11 +13,39 @@ let valyeInNumber=Number(score)
 
 let isLoggedIn="vibhor"
 let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 //1=>true;0=>false
 //""=>false
 //"vibhor"=>true
 
 let someNumber=33
 let stringNumber=String(someNumber)
-console.log(stringNumber)
+//console.log(stringNumber);
+
+
+//************Operations**********//
+let value=3
+let negvalue=-value
+//console.log(negvalue);
+
+let str1="hello"
+let str2=" vibhor"
+let str3=str1+str2
+// console.log(str3)
+
+// console.log("1"+2);
+// console.log(1+"2")
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log((3+4)*5%3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1,num2,num3
+num1=num2=num3=2+2
+
+let gameCounter=100
+++gameCounter;
+console.log(gameCounter);
